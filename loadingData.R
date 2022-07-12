97c426e56345ef784868108dbf069e7754fa4110
sarmapar <smp3800@gmail.com>
2022-07-12 16:33:14 -0400

    Adding plot

--- loadingData.R
+++ loadingData.R
@@ -1,10 +1,18 @@
+#### Load in required libraries ####
+library(ggplot2)
+
 #### Load in csv file ####
 x <- 5
 
 hero_data <- read.csv("data/heroes_information.csv")
 View(hero_data)
 
+## Practice plotting with your dataset
+ggplot(data = hero_data, aes(x = Height, y = Weight)) +
+  geom_point() +
+  scale_x_continuous(breaks = seq(from = 0, to = 1000, by = 100))
 
+hero_data$name[1:1000]
 
-
+x = as.factor(year)
 
