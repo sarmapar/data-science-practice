f6793d8c28ca3e74a8cb61185416b2e2744c925a
sarmapar <smp3800@gmail.com>
2022-07-14 13:44:47 -0400

    cleaning hero heights and creating subsets

---
+++ exploratory_analysis.R
@@ -0,0 +1,27 @@
+## Practice plotting with your dataset
+ggplot(data = hero_data, aes(x = Height, y = Weight)) +
+  geom_point() +
+  scale_x_continuous(breaks = seq(from = 0, to = 1000, by = 100))
+
+
+## Practice subsetting data
+# use a combination of 
+# filter, select, rename, mutate, arrange, summarise, group_by, sample, and/or slice 
+# create a visualization using your new subset of data
+
+View(hero_data)
+names(hero_data)
+
+plot <- hero_data %>%
+          filter(Height != -99) %>%
+          ggplot(aes(x = as.numeric(Height))) +
+          geom_density()
+
+plot
+
+
+new_data <- filter(hero_data, Height != -99)
+ggplot(new_data, aes(x = as.numeric(Height))) +
+  geom_density()
+
+hero_data$Height <- sub(-99, NA, hero_data$Height)
