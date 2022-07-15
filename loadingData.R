432dac8597cbe621510cf0fb973fdefc8096ae52
sarmapar <smp3800@gmail.com>
2022-07-15 15:56:06 -0400

    Added saving R object and RDS file

--- loadingData.R
+++ loadingData.R
@@ -2,17 +2,12 @@
 library(ggplot2)
 
 #### Load in csv file ####
-x <- 5
-
 hero_data <- read.csv("data/heroes_information.csv")
+
 View(hero_data)
 
-## Practice plotting with your dataset
-ggplot(data = hero_data, aes(x = Height, y = Weight)) +
-  geom_point() +
-  scale_x_continuous(breaks = seq(from = 0, to = 1000, by = 100))
+#### Save R object as a file ####
+saveRDS(hero_data, "data/hero_data.RDS")
 
-hero_data$name[1:1000]
 
-x = as.factor(year)
 
